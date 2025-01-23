3beee51d36c0faab7b3cd42bf1addd34c3a003ee
Christopher Cartagena <christophercartagena@Christophers-MacBook-Pro.local>
2025-01-23 12:37:28 -0600

    initializing repository

---
+++ src/main/java/com/footbal/PremierLeagueStates/PremierLeagueStatesApplication.java
@@ -0,0 +1,13 @@
+package com.footbal.PremierLeagueStates;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PremierLeagueStatesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PremierLeagueStatesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/footbal/PremierLeagueStates/PremierLeagueStatesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.footbal.PremierLeagueStates;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PremierLeagueStatesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
